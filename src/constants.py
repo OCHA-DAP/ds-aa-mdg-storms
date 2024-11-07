8f7d333d0a4c1b86cb3a3011babeaa1a015d4fc1
Pauline Wairimu <paunimo@gmail.com>
2024-11-07 12:21:03 +0300

    updating constants with conversion factor

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,19 @@
 import os
 KNOTS2MS = 0.514444
+kmh_to_knots = 0.5399568 # Conversion factor from km/h to knots
 THRESHOLD_SPEED_OPT1 = 118
 THRESHOLD_SPEED_OPT2 = 166
 ADMS = [
-   "Diana", "Sava", "Sofia", "Boeny", "Analanjirofo", "Ambatosoa", "Alaotra Mangoro", "Atsinanana", "Vatovavy", "Fitovinany", "Atsimo Atsinanana" 
-]
-ADMS2 = [
-    
+    "Diana",
+    "Sava",
+    "Sofia",
+    "Boeny",
+    "Analanjirofo",
+    "Ambatosoa",
+    "Alaotra Mangoro",
+    "Atsinanana",
+    "Vatovavy Fitovinany",
+    "Atsimo Atsinanana",
 ]
 AA_DATA_DIR = os.getenv("AA_DATA_DIR")
 AA_DATA_DIR_NEW = os.getenv("AA_DATA_DIR_NEW")

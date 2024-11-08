5902c289111acbe55501c02aabffc76ee0930fde
Pauline Wairimu <paunimo@gmail.com>
2024-11-08 11:37:18 +0300

    adding constants

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,9 @@ KNOTS2MS = 0.514444
 kmh_to_knots = 0.5399568 # Conversion factor from km/h to knots
 THRESHOLD_SPEED_OPT1 = 118
 THRESHOLD_SPEED_OPT2 = 166
+buffer = 100 #in km
+mdg_epsg = 29702 # for buffer conversion
+minute_to_10_minute_conversion_factor = 0.88 # converting 1-minute wind speeds to 10-minute
 ADMS = [
     "Diana",
     "Sava",

e9893ab2af8189a49b637a95df06a86d7fc87680
Pauline Wairimu <paunimo@gmail.com>
2024-12-16 10:06:08 +0300

    updating admin bound function

--- src/datasources/codab.py
+++ src/datasources/codab.py
@@ -9,15 +9,15 @@ AA_DATA_DIR = os.getenv("AA_DATA_DIR")
 ADMS = []
 
 def load_codab(admin_level: int = 0):
-    if not admin_level == 0:
-        raise ValueError("Only admin level 0 is supported")
-    adm1_path = (
+    if not admin_level in [0, 1, 2]:
+        raise ValueError("Only admin level 0, 1 and 2 is supported")
+    adm_path = (
         Path(AA_DATA_DIR)
         / "public"
         / "raw"
         / "mdg"
         / "cod_ab"
-        / "mdg_admbnda_adm0_BNGRC_OCHA_20181031.shp"
+        / f"mdg_admbnda_adm{admin_level}_BNGRC_OCHA_20181031.shp"
     )
-    gdf = gpd.read_file(adm1_path)
+    gdf = gpd.read_file(adm_path)
     return gdf

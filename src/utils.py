ffe7904b50840c378222c0bca3030a10d7e1869b
Pauline Wairimu <paunimo@gmail.com>
2024-12-16 22:55:24 +0300

    moving functions to utils

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from src import constants
+
 def speed2numcat(speed: float) -> int:
     """Convert wind speed in knots to numerical cyclone category using
     South-West Indian Ocean cyclone scale.
@@ -66,3 +69,47 @@ def categorize_cyclone(wind_speed):
         return "Tropical Depression"
     else:
         return "Tropical Disturbance"
+    
+def calculate_storm_return_period(
+    df, wind_speed_kmh_min, wind_speed_kmh_max, start_year
+):
+    # Convert the given speed from km/h to knots
+    min_speed_knots = wind_speed_kmh_min * constants.kmh_to_knots
+    max_speed_knots = wind_speed_kmh_max * constants.kmh_to_knots
+
+    # Extract the year from the 'ISO_TIME' column
+    df["year"] = df["ISO_TIME"].apply(
+        lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S").year
+    )
+
+    # Filter the DataFrame for records from the start year and with wind speed above the threshold
+    df_filtered = df[
+        (df["year"] >= start_year)
+        & (df["REU_WIND"] >= min_speed_knots)
+        & (df["REU_WIND"] <= max_speed_knots)
+    ]
+
+    # Count unique storms
+    unique_storms = df_filtered["NAME"].nunique()
+    unique_seasons = df_filtered["Season"].nunique()
+
+    # Calculate the total number of years in the filtered DataFrame
+    yr_len = 2024 - start_year + 1
+
+    # Calculate the combined return period
+    combined_return_period = yr_len / unique_seasons
+
+    print(
+        f"The combined return period of storms over {wind_speed_kmh_min}km/h is 1-in-{round(combined_return_period, 1)} years."
+    )
+
+def classify_scenario(deplacees_sum):
+    if 1000 <= deplacees_sum <= 10000:
+        return "Scenario 1"
+    elif 10000 < deplacees_sum <= 25000:
+        return "Scenario 2"
+    elif deplacees_sum > 25000:
+        return "Scenario 3"
+    else:
+        return "No Scenario"
+
